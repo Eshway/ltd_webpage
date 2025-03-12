0ae9a9c8d7ba8a4fc1fae718e90521a7aeb0c554
Eshu Koli <eshukoli@Eshus-MacBook-Air.local>
2025-03-12 19:54:09 +0530

    Favicon Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,11 @@ export default function RootLayout({
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <meta name="apple-mobile-web-app-title" content="LTD" />
+        <meta name="application-name" content="LTD" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
