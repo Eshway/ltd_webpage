ecda7183d411941041172e7f31bc46b0474f25ba
Eshu Koli <eshukoli@Eshus-MacBook-Air.local>
2025-03-12 19:59:40 +0530

    512x512 and 96x96 icons added

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,7 +67,9 @@ export default function RootLayout({
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <meta name="apple-mobile-web-app-title" content="LTD" />
         <meta name="application-name" content="LTD" />
